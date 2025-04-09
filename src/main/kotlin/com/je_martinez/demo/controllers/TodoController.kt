4620bd3b38b6c0d23ecdb59497ad4c4d7116ea1c
Jose Martinez <josemartinezchevez@gmail.com>
2025-04-09 08:53:21 -0600

    fix: validated annotation

--- src/main/kotlin/com/je_martinez/demo/controllers/TodoController.kt
+++ src/main/kotlin/com/je_martinez/demo/controllers/TodoController.kt
@@ -17,7 +17,7 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import java.time.Instant
 
-//@Validated
+@Validated
 @RestController
 @RequestMapping("/api/todos")
 class TodoController (private val repository: TodoRepository) {
