6147f94d1e9d204d96a6e88de9825d01c4dae7d5
Jose Martinez <josemartinezchevez@gmail.com>
2025-04-10 11:55:23 -0600

    fix: remove unused code

--- src/main/kotlin/com/je_martinez/demo/guards/TodoOwnershipEvaluator.kt
+++ src/main/kotlin/com/je_martinez/demo/guards/TodoOwnershipEvaluator.kt
@@ -4,7 +4,6 @@ import com.je_martinez.demo.database.repository.TodoRepository
 import com.je_martinez.demo.exceptions.TodoExceptions
 import com.je_martinez.demo.validators.HexString
 import org.bson.types.ObjectId
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.validation.annotation.Validated
 
@@ -14,7 +13,6 @@ class TodoOwnershipEvaluator(
     private val repository: TodoRepository
 ){
     fun isOwner(@HexString id: String, userId: String): Boolean{
-        val principal = SecurityContextHolder.getContext().authentication.principal
         val todo = repository.findById(ObjectId(id)).orElseThrow{
             TodoExceptions.notFound(id)
         }
