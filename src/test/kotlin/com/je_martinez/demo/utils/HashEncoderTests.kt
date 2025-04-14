5322c870c78cfc33f09d864ced02ca72487b0c81
Jose Martinez <josemartinezchevez@gmail.com>
2025-04-14 10:06:55 -0600

    test: wip uni testing

---
+++ src/test/kotlin/com/je_martinez/demo/utils/HashEncoderTests.kt
@@ -0,0 +1,34 @@
+package com.je_martinez.demo.utils
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertInstanceOf
+import kotlin.test.assertEquals
+
+class HashEncoderTests {
+
+    @Test
+    fun `Encode Hash should return a String`(){
+        val result = HashEncoder.encode("A random string to encode")
+        assertInstanceOf<String>(result)
+    }
+
+    @Test
+    fun `Hash Encoder should return a false if strings doesnt match`(){
+        val result = HashEncoder.encode("A random string to encode")
+
+        val matched = HashEncoder.matches("Hello", result)
+
+        assertInstanceOf<Boolean>(matched)
+        assertEquals(false, matched)
+    }
+
+    @Test
+    fun `Hash Encoder should return a true if strings match`(){
+        val text = "A random string to encode"
+        val result = HashEncoder.encode(text)
+        val matched = HashEncoder.matches(text, result)
+        assertInstanceOf<Boolean>(matched)
+        assertEquals(true, matched)
+    }
+
+}
---
+++ src/test/kotlin/com/je_martinez/demo/utils/HexUtilsTests.kt
@@ -0,0 +1,24 @@
+package com.je_martinez.demo.utils
+
+import org.bson.types.ObjectId
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertInstanceOf
+import kotlin.test.assertEquals
+
+class HexUtilsTests {
+
+    @Test
+    fun  `isValidHexString (true) - Test`(){
+        val result = HexUtils.isValidHexString(ObjectId().toHexString())
+        assertInstanceOf<Boolean>(result)
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun  `isValidHexString (false) - Test`(){
+        val result = HexUtils.isValidHexString("a-not-valid-test")
+        assertInstanceOf<Boolean>(result)
+        assertEquals(false, result)
+    }
+
+}
