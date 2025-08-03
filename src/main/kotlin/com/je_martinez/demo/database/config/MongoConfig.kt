d866fc1739e0b20a19241523434ef3027a74da81
Jose E. Martinez <josemartinezchevez@gmail.com>
2025-08-02 20:31:00 -0600

    fix: database build client

--- src/main/kotlin/com/je_martinez/demo/database/config/MongoConfig.kt
+++ src/main/kotlin/com/je_martinez/demo/database/config/MongoConfig.kt
@@ -1,32 +1,34 @@
 package com.je_martinez.demo.database.config
 
-import org.springframework.beans.factory.annotation.Autowired
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration
 
 @Configuration
 class MongoConfig(
-    @Autowired private val env: Environment
-): AbstractMongoClientConfiguration() {
-
-    companion object{
-        fun  getDatabaseName(uri:String):String? {
-            try{
-                val connectionString = com.mongodb.ConnectionString(uri)
-                print(connectionString.database)
-                return connectionString.database
-            }catch (_: Exception){
-                return null;
-            }
-        }
-    }
+    private val env: Environment // Constructor injection for Environment
+) : AbstractMongoClientConfiguration() {
 
     override fun getDatabaseName(): String {
-        return getDatabaseName(env.getProperty("spring.data.mongodb.uri") ?: "") ?: throw Exception("It required a valid database connection")
+        // Try to get the DB name from "spring.data.mongodb.database"
+        val dbFromProp = env.getProperty("spring.data.mongodb.database")
+        if (!dbFromProp.isNullOrBlank()) return dbFromProp
+
+        // If the database name is not explicitly set, parse it from the URI
+        val uri = env.getRequiredProperty("spring.data.mongodb.uri")
+        val cs = com.mongodb.ConnectionString(uri)
+        return cs.database ?: throw IllegalStateException(
+            "The URI does not include the database name and 'spring.data.mongodb.database' was not set"
+        )
     }
 
-    override fun autoIndexCreation(): Boolean {
-        return true
+    override fun mongoClient(): MongoClient {
+        // Explicitly create the MongoClient using the URI from environment variables
+        val uri = env.getRequiredProperty("spring.data.mongodb.uri")
+        return MongoClients.create(uri)
     }
+
+    override fun autoIndexCreation(): Boolean = true // Enable automatic index creation
 }
